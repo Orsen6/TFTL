c6a7c0e3a38631815f4cea12334ad8d182f457cb
Arsen <orsenkul@gmail.com>
2022-02-06 12:20:33 +0200

    added circle anim & footer

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.querySelector(".header_link-logo").classList.add("header_link-logo--loaded");
 	setTimeout(function () {
 		document.querySelector(".header_link-logo--sec").remove();
-	}, 1700);
+	}, 500);
 });
+
 
 document.querySelector(".header_inner-select").addEventListener('mouseover', function (event) {
 	event.stopPropagation;
 	document.querySelector(".header_inner-list").classList.toggle("header_inner-list--active");
 });
+
+const domLanguages = document.querySelectorAll(".header_inner-link");
+domLanguages.forEach((i) => {
+	i.addEventListener('click', () => {
+		document.querySelector(".header_inner-list").classList.toggle("header_inner-list--active");
+	})
+})
